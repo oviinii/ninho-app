caa3bb62b90ec6ee2a15cd240f45f91f2b413ff1
Vinicius Arruda <viniciusarruda@MacBook-Air-de-Vinicius.local>
2026-07-23 23:08:08 -0300

    fix: switch to npm and remove problematic dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,9 +40,5 @@ export default {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [],
 }
